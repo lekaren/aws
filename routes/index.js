f42bea6d61923cbd209c3518a93f42a43f37dd6b
Dabin <cpdnql@naver.com>
2020-10-30 14:30:33 +0900

    날짜 수정

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let a = []
-  Axios.get('https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=0fdf1d1c60aa4905a6587398591425a7&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530336&MLSV_YMD=20201029')
+  Axios.get('https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=0fdf1d1c60aa4905a6587398591425a7&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530336&MLSV_YMD=20201030')
   .then(res => res.data.mealServiceDietInfo[1].row[0].DDISH_NM)
   .then(res => {
     var test = res.toString();
