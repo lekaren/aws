47eaa960399b9f04398b55cfc1f06c36d2abdc6e
Dabin <cpdnql@naver.com>
2020-10-30 16:33:28 +0900

    로컬 테스트

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,9 @@
-const { default: Axios } = require('axios');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let a = []
-  Axios.get('https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=0fdf1d1c60aa4905a6587398591425a7&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530336&MLSV_YMD=20201030')
-  .then(res => res.data.mealServiceDietInfo[1].row[0].DDISH_NM)
-  .then(res => {
-    var test = res.toString();
-    for(let i = 0; i < test.split("<br/>").length; i++){
-      a.push(test.split("<br/>")[i].match(/[^(삼일)]/g).join('').match(/[가-힣]/g).join(''))
-    }
-  })
-  .then(response => res.render('index', { title: 'Express' , data:a }))
+  res.send('점심메뉴 조회');
 });
 
 module.exports = router;
---
+++ routes/launch.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+const { default: Axios } = require('axios');
+
+require('date-utils');
+var newDate = new Date();
+var time = newDate.toFormat('YYYYMMDD');
+
+router.get('/', function(req, res, next) {
+    let a = []
+    Axios.get(`https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=0fdf1d1c60aa4905a6587398591425a7&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530336&MLSV_YMD=${time}`)
+    .then(res => res.data.mealServiceDietInfo[1].row[0].DDISH_NM)
+    .then(res => {
+      var test = res.toString();
+      for(let i = 0; i < test.split("<br/>").length; i++){
+        a.push(test.split("<br/>")[i].match(/[^(삼일)]/g).join('').match(/[가-힣]/g).join(''))
+      }
+    })
+    .then(response => res.render('index', { title: 'Express' , data:a }))
+  });
+  
+
+module.exports = router;
