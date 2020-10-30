05c0fb6b534bd8be8502ef21b52155e0ead1934a
Dabin <cpdnql@naver.com>
2020-10-30 10:47:20 +0900

    급식 메뉴

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
+const { default: Axios } = require('axios');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let a = []
+  Axios.get('https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=0fdf1d1c60aa4905a6587398591425a7&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530336&MLSV_YMD=20201029')
+  .then(res => res.data.mealServiceDietInfo[1].row[0].DDISH_NM)
+  .then(res => {
+    var test = res.toString();
+    for(let i = 0; i < test.split("<br/>").length; i++){
+      a.push(test.split("<br/>")[i].match(/[^(삼일)]/g).join('').match(/[가-힣]/g).join(''))
+    }
+  })
+  .then(response => res.render('index', { title: 'Express' , data:a }))
 });
 
 module.exports = router;
